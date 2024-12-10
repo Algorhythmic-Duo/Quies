d9d2be7df22f67f7594a6f1b5919e7c2ce4a551d
mathews <mathewsvinoy@gmail.com>
2024-12-09 23:18:18 -0500

    complete

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:quies/bloc/Button/button_bloc.dart';
-import 'package:quies/bloc/progressbar/progressbar_bloc.dart';
 import 'package:quies/pages/splishscreen.dart';
 
 void main() async {
@@ -16,11 +15,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MultiBlocProvider(
-      providers: [
-        BlocProvider(create: (context) => ButtonBloc()),
-        BlocProvider(create: (context) => ProgressbarBloc())
-      ],
+    return BlocProvider(
+      create: (context) => ButtonBloc(),
       child: MaterialApp(
         title: 'Flutter Demo',
         theme: ThemeData(
